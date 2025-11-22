2373ae1fb0673edbba9c45962554fc0cac4dd44e
ksj030731 <232139331+ksj030731@users.noreply.github.com>
2025-11-22 22:29:52 +0900

    test
    .

--- src/main/java/chatting/config/SecurityConfig.java
+++ src/main/java/chatting/config/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
-
+//test
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
